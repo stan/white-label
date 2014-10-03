2ac7740508f3915af60cf635582d51eabdbd4abd
stan Mazurek <stan@mazurek.me>
2014-10-03 01:30:56 +0100

    GitLab 7.3.2

---
+++ app/helpers/appearances_helper.rb
@@ -0,0 +1,17 @@
+module AppearancesHelper
+  def brand_item
+    true
+  end
+
+  def brand_title
+    'White Label'
+  end
+
+  def brand_image
+    image_tag 'brand_logo.png'
+  end
+
+  def brand_text
+    nil
+  end
+end
